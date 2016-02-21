dd7a40bc637624abc6e9e362ea7104c1b545ab1e
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2016-02-21 11:11:16 +0000

    chapter 8 code

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -8,18 +8,18 @@ require "rulers/file_model"
 module Rulers
   class Application
     def call(env)
-      klass, act = get_controller_and_action(env)
-      controller = klass.new(env)
-      text = controller.send(act)
-      r = controller.get_response
-      if r
-        [r.status, r.headers, [r.body].flatten]
-      else
-        [200, {'Content-Type' => 'text/html'}, [text]]
-      end
-     rescue UnknownControllerError
-       index_content = File.readlines("public/index.html").join
-       [200, {'Location' => 'index.html' }, [index_content]]
-     end
+      rack_app = get_rack_app(env)
+      rack_app.call(env)
+    end
+
+    def route(&block)
+      @route_obj ||= RouteObject.new
+      @route_obj.instance_eval(&block)
+    end
+
+    def get_rack_app(env)
+      raise "No routes!" unless @route_obj
+      @route_obj.check_url env["PATH_INFO"]
+    end
   end
 end
--- rulers/lib/rulers/controller.rb
+++ rulers/lib/rulers/controller.rb
@@ -7,14 +7,31 @@ module Rulers
 
     def initialize(env)
       @env = env
+      @routing_params = {}
     end
 
     def request
       @request ||= Rack::Request.new(@env)
     end
 
+    def dispatch(action, routing_params = {})
+      @routing_params = routing_params
+      text = self.send(action)
+      r = get_response
+      if r
+        [r.status, r.headers, [r.body].flatten]
+      else
+        [200, {'Content-Type' => 'text/html'},
+         [text].flatten]
+      end
+    end
+
+    def self.action(act, rp = {})
+      proc { |e| self.new(e).dispatch(act, rp) }
+    end
+
     def params
-      request.params
+      request.params.merge @routing_params
     end
 
     def env
